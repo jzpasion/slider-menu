b155256d734139f46c5e55291dc771f77fd11209
jzpasion <you@example.com>
2019-06-27 16:21:36 +0800

    updated

--- carousel.js
+++ carousel.js
@@ -1,35 +1,3 @@
-// var slideIndex = 1;
-// showSlides(slideIndex);
-
-// function SlideNav(n) {
-//   showSlides((slideIndex += n));
-// }
-
-// function currentSlide(n) {
-//   showSlides((slideIndex = n));
-// }
-
-// function showSlides(n) {
-//   var i;
-//   var slides = document.getElementsByClassName("images");
-//   var dots = document.getElementsByClassName("dot");
-//   for (var x = 0; x < slides.length; x++) {}
-//   if (n > slides.length) {
-//     slideIndex = 1;
-//   }
-//   if (n < 1) {
-//     slideIndex = slides.length;
-//   }
-//   for (i = 0; i < slides.length; i++) {
-//     slides[i].style.display = "none";
-//   }
-//   for (i = 0; i < dots.length; i++) {
-//     dots[i].className = dots[i].className.replace(" active", "");
-//   }
-//   slides[slideIndex - 1].style.display = "block";
-//   dots[slideIndex - 1].className += " active";
-// }
-
 var slideIndex = 0;
 SlideShow();
 
